89d512bd4e16058d92d57fe07754cc79efc06012
tirthamoulib <btirthamouli@gmail.com>
2020-01-19 20:13:09 +0530

    Added readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Event Loop
+
+This is an implementation of the event loop in golang.
+
+#### The execution starts from the src/start/start.go in the start func
--- src/start.go
+++ src/start.go
@@ -6,15 +6,19 @@ import (
 	"github.com/tirthamoulib/event_loop/src/lib"
 )
 
+func createTimeLoop(num int) {
+	fmt.Println(num)
+	if num == 0 {
+		return
+	}
+	lib.SetTimeout(1000, func() {
+		createTimeLoop(num - 1)
+	})
+}
+
 // Start - start of the program
 func Start() {
 	fmt.Println("Hello")
-	for i := 0; i < 1000000; i++ {
-		j := i
-		lib.SetTimeout(0, func() {
-			fmt.Println(j, "Sup")
-		})
-	}
-
+	createTimeLoop(10)
 	fmt.Println("World")
 }
